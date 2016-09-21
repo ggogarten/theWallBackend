f3d7dfda6f64aaf1c8290430ae44c5f27e245f5a
George G <ggogarten@gmail.com>
2016-09-21 19:05:24 -0400

    added users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916164955) do
+ActiveRecord::Schema.define(version: 20160921230403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "auth_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "wall_posts", force: :cascade do |t|
     t.string   "postMsg"
